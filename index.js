df5b0395d2365980d66fbb66467ac92276fa469d
SheilaSusan <susansheila08@gmail.com>
2025-05-03 09:37:32 +0300

    Password generator
    Generates two random passwords 15 characters long and displays them

---
+++ index.js
@@ -0,0 +1,19 @@
+let password1El = document.getElementById("password1-el")
+let password2El = document.getElementById("password2-el")
+
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+function password(){
+    let password = ""
+    for(let i = 0; i < 15; i++){
+        let randomIndex = Math.floor(Math.random() * characters.length)
+        password += characters[randomIndex]
+    }
+  return password
+}
+
+function displayPasswords(){
+  password1El.textContent = password()
+  password2El.textContent = password()
+}
