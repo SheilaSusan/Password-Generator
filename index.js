fdbeda2a3e98d8a2edb0bd99c3442857ad52de86
SheilaSusan <susansheila08@gmail.com>
2025-05-18 18:24:29 +0300

    Modified the general layout. Added the save and delete buttons Used JS to make the buttons interactive Added an empty button to display the selected password Modified JS by using localStorage to save the password

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-let password1El = document.getElementById("password1-el")
-let password2El = document.getElementById("password2-el")
+const password1El = document.getElementById("password1-el")
+const password2El = document.getElementById("password2-el")
+const selectedPassword = document.getElementById("selected-password")
+const saveBtn = document.getElementById("save-btn")
+const deleteBtn = document.getElementById("delete-btn")
 
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = [
+  "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+  "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", 
+  "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+  "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+  "/"
+];
 
 function password(){
     let password = ""
@@ -13,7 +21,46 @@ function password(){
   return password
 }
 
+//Displays the passwords
 function displayPasswords(){
   password1El.textContent = password()
   password2El.textContent = password()
 }
+
+function passwordSelection (element){
+  const password = element.textContent
+  if (!password) return
+
+  selectedPassword.innerHTML = " "
+
+  const selectedBtn = document.createElement("button")
+  selectedBtn.className = "password-btn"
+  selectedBtn.textContent = password
+
+  selectedPassword.appendChild(selectedBtn)
+}
+
+password1El.addEventListener("click", function(){
+  passwordSelection(password1El)
+})
+
+password2El.addEventListener("click", function(){
+  passwordSelection(password2El)
+})
+
+saveBtn.addEventListener("click", function(){
+  const selectedPassword = document.querySelector("#selected-password button")
+  if(!selectedPassword){
+    alert ("No password selected!")
+    return
+  }
+
+  //Saves password in the local storage of browser memory
+  localStorage.setItem("savedPassword", selectedPassword.textContent)
+  alert("Password saved")
+})
+
+//deletes the selected password
+deleteBtn.addEventListener("click", function(){
+  selectedPassword.innerHTML = ""
+})
